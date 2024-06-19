09714307408cefb1152d6d6306fc614141181d14
Jolayus <cabangonjulius86@gmail.com>
2024-06-19 23:42:43 +0800

    Added Pages into Routes and MainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,116 +1,59 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout
+    view="lHh lpr lFf"
+    container
+    style="height: 100vh"
+    class="shadow-2 rounded-borders"
+  >
     <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+      <q-toolbar style="width: 80vw; margin: 0 auto">
+        <q-avatar>
+          <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+        </q-avatar>
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+        <q-toolbar-title>Quasar Framework</q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-btn flat round dense icon="whatshot" />
+        <q-btn flat round dense icon="whatshot" />
+        <q-btn flat round dense icon="whatshot" />
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
-      <router-view />
+      <IndexPage style="border: 5px solid red" />
+      <AboutPage style="border: 5px solid blue" />
+      <ContactsPage style="border: 5px solid green" />
+      <ProjectsPage style="border: 5px solid yellow" />
+      <ExperiencePage style="border: 5px solid black" />
     </q-page-container>
+
+    <q-footer elevated>
+      <q-toolbar>
+        <q-toolbar-title> Footer </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
   </q-layout>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+import { defineComponent } from "vue";
+import IndexPage from "src/pages/IndexPage.vue";
+import AboutPage from "src/pages/AboutPage.vue";
+import ContactsPage from "src/pages/ContactsPage.vue";
+import ProjectsPage from "src/pages/ProjectsPage.vue";
+import ExperiencePage from "src/pages/ExperiencePage.vue";
 
 export default defineComponent({
-  name: 'MainLayout',
-
+  name: "MainLayout",
   components: {
-    EssentialLink
+    IndexPage,
+    AboutPage,
+    ContactsPage,
+    ProjectsPage,
+    ExperiencePage,
   },
-
-  setup () {
-    const leftDrawerOpen = ref(false)
-
-    return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
+  setup() {
+    return { IndexPage, AboutPage, ContactsPage, ProjectsPage, ExperiencePage };
+  },
+});
 </script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,49 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      {
+        path: "",
+        name: "index-page",
+        component: () => import("pages/IndexPage.vue"),
+      },
+      {
+        path: "",
+        name: "about-page",
+        component: () => import("pages/AboutPage.vue"),
+      },
+      {
+        path: "",
+        name: "experience-page",
+        component: () => import("pages/ExperiencePage.vue"),
+      },
+      {
+        path: "",
+        name: "projects-page",
+        component: () => import("pages/ProjectsPage.vue"),
+        // children: [
+        //   {
+        //     path: ":id",
+        //     name: "project-page",
+        //     component: () => import("pages/ProjectPage.vue"),
+        //   },
+        // ],
+      },
+      {
+        path: "",
+        name: "contacts-page",
+        component: () => import("pages/ContactsPage.vue"),
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
